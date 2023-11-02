e36cc46d06695a1f89c76e3fb456106d679d3323
MrLinusMeow <>
2023-11-02 01:15:58 +0100

    added flexible output feature

--- nc.c
+++ nc.c
@@ -2,33 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 
-long convert(char* string, char base){
+char* convert(char* string, char baseIn, char baseOut){
 	long n = 1;
 	long counter = 0;
+	char valid = 1;
+	char r = 0;
+	char output[256] = {};
+	char temp;
+	if(baseIn == baseOut) return string;
+	
 	for(short i = strlen(string)-1; i >= 0; --i){
 		if(string[i] >= 'A' && string[i] <= 'Z') string[i] += 0x20;
 		
 		if(string[i] >= '0' && string[i] <= '9'){
-			if(string[i]-0x30 < base) counter += n*(string[i]-0x30); 
-			else {printf("number: \'%c\' is not in base %i.\n", string[i], base); return -1;}
+			if(string[i]-0x30 < baseIn) counter += n*(string[i]-0x30);
+			else{
+				valid = 0;
+				fprintf(stderr, "\e[1;31m[ERROR]\e[0m Number: \'%c\' is not in base: %i.\n", string[i], baseIn);
+			}
+		}else if(string[i] >= 'a' && string[i] <= 'z'){
+			if(string[i]-0x57 < baseIn) counter += n*(string[i]-0x57);
+			else{
+				valid = 0;
+				fprintf(stderr, "\e[1;31m[ERROR]\e[0m Letter: \'%c\' is not in base: %i.\n", string[i], baseIn);
+			}
+		}else {
+			valid = 0;
+			fprintf(stderr, "\e[1;31m[ERROR]\e[0m Invalid character \'%c\'.\n\e[1;36m[HINT]\e[0m Valid characters are: 0-9, A-Z, a-z.", string[i]);
 		}
-		else if(string[i] >= 'a' && string[i] <= 'z'){
-			if(string[i]-0x57 < base) counter += n*(string[i]-0x57);
-			else{ printf("number: \'%c\' is not in base %i.\n", string[i], base); return -1;}
+		n *= baseIn;
+	}
+	for(short j = 0;; ++j){
+		(char)r = counter % baseOut;
+		if(r < 10){
+			output[j] = r+0x30;
+		}else{
+			output[j] = r+0x57;
 		}
-		else return -1;
-		n *= base;
-	} return counter;
+		counter -= r;
+		counter /= baseOut;
+		if (counter == 0) {r = j; break;}
+	}
+	for(short x = 0, y = r; x != r; ++x, --y){
+		temp = output[x];
+		output[x] = output[y];
+		output[y] = temp;
+	}
+	return output;
 };
 
 int main(int argc, char **argv, char **envp){
-	char base = 16;
-
+	char inputBase = 10;
+	char outputBase = 16;
 	if(argv[1] == '\0'){ 
-		printf("No argument");
+		printf("No argument\n");
 		return -1;
 	}
 	for(int i = 1; argv[i] != '\0'; ++i) {
-		printf("%b\n", convert(argv[i], base));
+		printf("%s\n", convert(argv[i], inputBase, outputBase));
 	} return 0;
 }
